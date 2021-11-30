0034dbef7bc4744a8f52cd4b6c295f8669a87288
Sau <songsterr2013@gmail.com>
2021-11-30 16:01:52 +0800

    SOME NAME CHANGED AND FIRSTLY EDITED

--- market/forms.py
+++ market/forms.py
@@ -9,7 +9,7 @@ class RegisterForm(FlaskForm):
     # When you inherit a class,
     # FlaskForm will care all functions that you create and name prefix with 'validate',
     # after the '_' it is required to name with just the same as val as you create,
-    # like 'username', once it found ,it will pass it into parameter and excute sth.
+    # like 'username', once it found ,it will pass it into parameter and execute sth.
 
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
@@ -21,7 +21,7 @@ class RegisterForm(FlaskForm):
         if email_address:
             raise ValidationError('Email address already exists! Please try a different email address')
 
-    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    username = StringField(label='帳號:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
@@ -29,6 +29,6 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label='User Name:', validators=[DataRequired()])
-    password = PasswordField(label='Password:', validators=[DataRequired()])
-    submit = SubmitField(label='Sign In')
+    username = StringField(label='請輸入帳號:', validators=[DataRequired()])
+    password = PasswordField(label='請輸入密碼:', validators=[DataRequired()])
+    submit = SubmitField(label='登入')
