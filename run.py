58530b4411b8ee4c34a7e4ee55f337a996d3cdde
songsterr2013 <59828375+songsterr2013@users.noreply.github.com>
2021-12-19 20:43:38 +0800

    V1

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from main import app
 
+from tornado.httpserver import HTTPServer
+from tornado.wsgi import WSGIContainer
+from tornado.ioloop import IOLoop
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8090)
+
+    s = HTTPServer(WSGIContainer(app))
+    s.listen(8090)  # 監聽 9900 端口
+    IOLoop.current().start()
+
+    #app.run(debug=True, host="0.0.0.0", port=8090)
